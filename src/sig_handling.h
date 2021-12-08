dbcf09f92bd4e3544056eed60da66422986a7202
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-12-08 16:28:41 +0100

    Improvements and refactoring.

--- src/sig_handling.h
+++ src/sig_handling.h
@@ -8,16 +8,54 @@
 #include <stdio.h>
 #include <time.h>
 
-extern int timeout_expired;
-extern int escape;
+#ifdef MRC_GPU
+#include "launch_parameters_gpu.cuh"
+#endif
+
+#ifdef POSIT
+extern const int POSIT_n;
+#endif
+
+#ifdef BOHM
+extern const int BOHM_alpha;
+extern const int BOHM_beta;
+#endif
+
 extern clock_t solve_tic;
 extern clock_t solve_toc;
 extern double solving_time;
-extern double max_heuristic_time;
-extern double min_heuristic_time;
-extern double avg_heuristic_time;
-extern double tot_heuristic_time;
+
+#if defined MRC || defined MRC_DYN || defined MRC_GPU
+extern double miracle_time;
+
+extern double max_inc_dec_lvl_time;
+extern double min_inc_dec_lvl_time;
+extern double avg_inc_dec_lvl_time;
+extern double tot_inc_dec_lvl_time;
+extern int num_inc_dec_lvl;
+
+extern double max_assign_time;
+extern double min_assign_time;
+extern double avg_assign_time;
+extern double tot_assign_time;
+extern int num_assign;
+
+extern double max_bj_time;
+extern double min_bj_time;
+extern double avg_bj_time;
+extern double tot_bj_time;
+extern int num_bj;
+#endif
+
+extern double max_heur_time;
+extern double min_heur_time;
+extern double avg_heur_time;
+extern double tot_heur_time;
 extern int num_heur;
+
+extern int timeout_expired;
+extern int escape;
+extern int timeout;
 
 void install_alarmhandler();
 void install_handler();
@@ -92,17 +130,87 @@ void print_stats() {
   printf("******************************************************************");
   printf("\n\n");
 
-  printf("Solving time: %f ms\n", solving_time);
-  printf("Maximum heuristic computation time: %f ms\n", max_heuristic_time);
-  printf("Minimum heuristic computation time: %f ms\n", min_heuristic_time);
-  avg_heuristic_time = tot_heuristic_time / num_heur;
-  printf("Average heuristic computation time: %f ms\n", avg_heuristic_time);
-  printf("Total heuristic computation time: %f ms\n", tot_heuristic_time);
-  printf("%% of the solving time used in the heuristic computation: %f %%\n",
-         (tot_heuristic_time * 100) / solving_time);
-  printf("Number of heuristic calls: %d\n", num_heur);
-  
+  if (timeout_expired) {
+    printf("Timeout expired: YES\n");
+  } else {
+    printf("Timeout expired: NO\n");
+  }
+
+  if (escape) {
+    printf("SIGINT captured: YES\n");
+  } else {
+    printf("SIGINT captured: NO\n");
+  }
+
+  printf("Timeout: %d s\n", timeout);
+#ifdef MRC_GPU
+  printf("Number of threads per block: %d\n", gpu_num_threads_per_block());
+#endif
+#ifdef POSIT
+  printf("POSIT n: %d\n", POSIT_n);
+#endif
+#ifdef BOHM
+  printf("BOHM alpha: %d\n", BOHM_alpha);
+  printf("BOHM beta: %d\n", BOHM_beta);
+#endif
   printf("\n");
+
+  printf("Solving time: %f ms\n", solving_time);
+  printf("\n");
+
+#if defined MRC || defined MRC_DYN || defined MRC_GPU
+  printf("MiraCle time: %f ms\n", miracle_time);
+  printf("%% of solving time used in MiraCle calls: %f %%\n",
+         (miracle_time * 100) / solving_time);
+  printf("\n");
+
+  printf("Maximum decision level increase time: %f ms\n", max_inc_dec_lvl_time);
+  printf("Minimum decision level increase time: %f ms\n", min_inc_dec_lvl_time);
+  avg_inc_dec_lvl_time = tot_inc_dec_lvl_time / num_inc_dec_lvl;
+  printf("Average decision level increase time: %f ms\n", avg_inc_dec_lvl_time);
+  printf("Total decision level increase time: %f ms\n", tot_inc_dec_lvl_time);
+  printf("%% of MiraCle time used in increase decision level calls: %f %%\n",
+         (tot_inc_dec_lvl_time * 100) / miracle_time);
+  printf("Number of increase decision level calls: %d\n", num_inc_dec_lvl);
+  printf("\n");
+
+  printf("Maximum assignment time: %f ms\n", max_assign_time);
+  printf("Minimum assignment time: %f ms\n", min_assign_time);
+  avg_assign_time = tot_assign_time / num_assign;
+  printf("Average assignment time: %f ms\n", avg_assign_time);
+  printf("Total assignment time: %f ms\n", tot_assign_time);
+  printf("%% of MiraCle time used in assignment calls: %f %%\n",
+         (tot_assign_time * 100) / miracle_time);
+  printf("Number of assignment calls: %d\n", num_assign);
+  printf("\n");
+
+  printf("Maximum backjumping time: %f ms\n", max_bj_time);
+  printf("Minimum backjumping time: %f ms\n", min_bj_time);
+  avg_bj_time = tot_bj_time / num_bj;
+  printf("Average backjumping time: %f ms\n", avg_bj_time);
+  printf("Total backjumping time: %f ms\n", tot_bj_time);
+  printf("%% of MiraCle time used in backjumping calls: %f %%\n",
+         (tot_bj_time * 100) / miracle_time);
+  printf("Number of backjumping calls: %d\n", num_bj);
+  printf("\n");
+#endif
+
+  printf("Maximum heuristic time: %f ms\n", max_heur_time);
+  printf("Minimum heuristic time: %f ms\n", min_heur_time);
+  avg_heur_time = tot_heur_time / num_heur;
+  printf("Average heuristic time: %f ms\n", avg_heur_time);
+  printf("Total heuristic time: %f ms\n", tot_heur_time);
+#if defined MRC || defined MRC_DYN || defined MRC_GPU
+  printf("%% of MiraCle time used in heuristic calls: %f %%\n",
+         (tot_heur_time * 100) / miracle_time);
+#endif
+#ifdef NO_MRC
+  printf("%% of solving time used in heuristic calls: %f %%\n",
+         (tot_heur_time * 100) / solving_time);
+#endif
+  printf("Number of heuristic calls: %d\n", num_heur);
+  printf("\n");
+
   printf("******************************************************************");
   printf("\n");
   printf("***********************    END STATS    **************************");
